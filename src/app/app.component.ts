0c016a7c4b5ded615569a4577964e3faee953693
Dennis Neuendorf <dene@netlight.com>
2017-10-31 12:20:47 +0100

    finished filterlist

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/Observable/of';
 import 'rxjs/add/Observable/combineLatest';
+import 'rxjs/add/operator/startWith';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,20 @@ import 'rxjs/add/Observable/combineLatest';
 export class AppComponent implements OnInit {
   title = 'app works!';
 
-  list: string[] = [
+  list$: Observable<string[]> = Observable.of([
     'Andreas',
     'Andrea',
     'Charles',
     'Dennis',
     'Sandro'
-  ];
+  ]);
 
   click$: Subject<string> = new Subject<string>();
 
-  filteredList$: Observable<string[]> = Observable.combineLatest(Observable.of(this.list),this.click$, (list: string[], filter: string) => list.filter(x => x.indexOf(filter) !== -1) );
+  /*filteredList$: Observable<string[]> = Observable
+    .combineLatest(this.list$, this.click$.startWith(''), (list: string[], filter: string) =>
+      list.filter(x => x.indexOf(filter) !== -1)
+    );*/
 
   ngOnInit() {
     this.click$.subscribe(x => console.log(x));
