82b44688c486365c66630601a16773b67deaec96
Dennis Neuendorf <dene@netlight.com>
2017-10-31 12:04:52 +0100

    fixed intro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Subject } from 'rxjs/Subject';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/Observable/of';
-import 'rxjs/add/Observable/combineLatest';
+import 'rxjs/add/operator/scan';
+import 'rxjs/add/operator/map';
 
 @Component({
   selector: 'app-root',
@@ -12,17 +13,9 @@ import 'rxjs/add/Observable/combineLatest';
 export class AppComponent implements OnInit {
   title = 'app works!';
 
-  list: string[] = [
-    'Andreas',
-    'Andrea',
-    'Charles',
-    'Dennis',
-    'Sandro'
-  ];
-
   click$: Subject<string> = new Subject<string>();
 
-  filteredList$: Observable<string[]> = Observable.combineLatest(Observable.of(this.list),this.click$, (list: string[], filter: string) => list.filter(x => x.indexOf(filter) !== -1) );
+  acc$: Observable<number> = this.click$.map(x => parseInt(x, 10)).scan((last, current) => last + current);
 
   ngOnInit() {
     this.click$.subscribe(x => console.log(x));
